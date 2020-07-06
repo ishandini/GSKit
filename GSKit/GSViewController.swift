72789172cb74cf6c412b9b62e4d666c51f3b7edf
ishan <ishan@jrcsoftware.com>
2020-07-06 21:09:56 +0530

    new lib added.

--- GSKit/GSViewController.swift
+++ GSKit/GSViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 open class GSViewController: UIViewController {
     public func changeBackground(withColor color: UIColor) {
         self.view.backgroundColor = color
+
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "Loading"
+        hud.show(in: self.view)
+        hud.dismiss(afterDelay: 3.0)
     }
 }
